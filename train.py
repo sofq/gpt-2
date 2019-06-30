fa17c13c46f116808f7388b648c8189bfa5726ad
sofq <49865170+sofq@users.noreply.github.com>
2019-06-30 11:20:11 +0700

    Update train.py

--- train.py
+++ train.py
@@ -190,16 +190,18 @@ def main():
                     feed_dict={context: args.batch_size * [context_tokens]})
                 for i in range(min(args.sample_num - index, args.batch_size)):
                     text = enc.decode(out[i])
-                    text = '======== SAMPLE {} ========\n{}\n'.format(
-                        index + 1, text)
+                    
                     t = ''
                     text.split()
                     for x in text.split():
                         for y in ['_A','_C','_E','_I','_L','_M','_N','_Nc','_Ny','_Np','_Nu','_P','_R','_S','_T','_V','_X','_F']:
                             if x.endswith(y):
                                 x = x.replace(y,'')
+                                x = x.replace('_','')
                         t += x + ' '
-                    text = t 
+                    text = t
+                    text = '======== SAMPLE {} ========\n{}\n'.format(
+                        index + 1, text)
                     all_text.append(text)
                     index += 1
             print(text)
