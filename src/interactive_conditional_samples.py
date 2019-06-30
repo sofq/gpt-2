055610ede6df934345f5d5fbdf1161672ea25130
sofq <49865170+sofq@users.noreply.github.com>
2019-06-30 13:10:57 +0700

    Update interactive_conditional_samples.py

--- src/interactive_conditional_samples.py
+++ src/interactive_conditional_samples.py
@@ -87,7 +87,7 @@ def interact_model(
                     t = ''
                     text.split()
                         for x in text.split():
-                            for y in ['_A','_C','_E','_I','_L','_M','_N','_Nc','_Ny','_Np','_Nu','_P','_R','_S','_T','_V','_X','_F','_B','_H']:
+                            for y in ['_A','_C','_E','_I','_L','_M','_N','_Nc','_Ny','_Np','_Nu','_Vc','_P','_R','_S','_T','_V','_X','_F','_B','_H']:
                                 if x.endswith(y):
                                     x = x.replace(y,'')
                                     x = x.repalce('_',' ')
